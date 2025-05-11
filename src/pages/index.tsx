786560da122e72cd8f7b8812057a8881be213057
tangyijie.666 <tangyijie.666@bytedance.com>
2025-05-11 20:07:27 +0800

    第一次

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,30 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import ThreeDCardDemo from '../components/CardContainer/3d-card-demo';
-import GlobeDemo from  '../components/globe/cGlobe';
 import Layout from '@theme/Layout';
-import {ShootingStars} from "@site/src/components/shooting-stars/shooting-stars";
-import {StarsBackground} from "@site/src/components/shooting-stars/stars-background";
+import { ShootingStars } from '@site/src/components/shooting-stars/shooting-stars';
+import { StarsBackground } from '@site/src/components/shooting-stars/stars-background';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-      <Layout>
-              <div className="tailwind">
-                  <div className="h-[40rem] rounded-md dark:bg-black flex flex-col items-center justify-center relative w-full">
-                      <ShootingStars />
-                      <StarsBackground />
-                      <ThreeDCardDemo/>
-                  </div>
-                  <GlobeDemo/>
+    const { siteConfig } = useDocusaurusContext();
 
-              </div>
+    return (
+        <Layout>
+            <div className="tailwind">
+                <div className="h-[40rem] rounded-md dark:bg-black flex flex-col items-center justify-center relative w-full">
+                    <ShootingStars />
+                    <StarsBackground />
+                    <ThreeDCardDemo />
+                </div>
 
-      </Layout>
-
-
-  );
+                <BrowserOnly>
+                    {() => {
+                        const GlobeDemo = require('../components/globe/cGlobe').default;
+                        return <GlobeDemo />;
+                    }}
+                </BrowserOnly>
+            </div>
+        </Layout>
+    );
 }
